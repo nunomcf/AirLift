a98743a9cd8a0faa698ff856598cded7dda5b322
Sérgio Gasalho <paulogasalho@ua.pt>
2021-05-22 15:34:12 +0100

    Shared Region Main

--- AirLift_assignment2/src/main/SharedRegionMain.java
+++ AirLift_assignment2/src/main/SharedRegionMain.java
@@ -4,8 +4,10 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.SocketTimeoutException;
 
-import common.ServerComm;
-import common.ServiceProvider;
+import common.*;
+import proxies.*;
+import sharedRegions.*;
+import stubs.RepositoryStub;
 
 /**
  * Launch shared region server
@@ -25,9 +27,30 @@ public class SharedRegionMain {
 	*/
 	public static void main(String[] args) throws SocketTimeoutException, ClassNotFoundException, NoSuchMethodException, NoSuchFieldException, InstantiationException, IllegalAccessException, InvocationTargetException {
 		// TODO Auto-generated method stub
+		
 		ServerComm serverCom, serverConn;
 		ServiceProvider serviceProvider;
 		String regionName = args[0].substring(0, 1).toUpperCase() + args[0].substring(1);
+		
+		Class<?> shRegClass = Class.forName("sharedRegions." + regionName);
+		Constructor<?> shRegConstructor = shRegClass.getConstructor(RepositoryStub.class);
+		SharedRegion sharedRegion = (SharedRegion) shRegConstructor.newInstance(new RepositoryStub());
+		
+		Class<?> proxyClass = Class.forName("proxies." + regionName + "Proxy");
+		Constructor<?> proxyConstructor = proxyClass.getConstructor(shRegClass);
+		SharedRegionProxy proxy = (SharedRegionProxy) proxyConstructor.newInstance(sharedRegion);
+
+		serverCom = new ServerComm(Parameters.class.getField(args[0].substring(0,1).toLowerCase()+args[0].substring(1) + "Port").getInt(null),1000);
+		serverCom.start();
+
+		while(proxy.getSimStatus()) {
+			try {
+				serverConn = serverCom.accept();
+				serviceProvider = new ServiceProvider(proxy, serverConn);
+				serviceProvider.start();
+			} catch (SocketTimeoutException e) {}
+		}
+		System.out.printf("%s: Bye!\n",regionName);
 		
 		
 
